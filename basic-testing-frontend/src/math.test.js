038bead2debb0904043acce96e85a50f05175d9d
Abolfazl-Khan <abolfazlkhan.m@gmail.com>
2022-06-18 23:38:23 +0430

    updated math.test to add AAA patern in  basic-testing-frontend

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -2,6 +2,15 @@ import { it, expect } from 'vitest';
 import { add } from './math';
 
 it('should be sum of all numbers in array', () => {
-  const testResult = add([1, 2, 3]);
-  expect(testResult).toBe(6);
+  //Arrange
+  const sampleArray = [1, 2, 3];
+
+  //Act
+  const testResult = add(sampleArray);
+  //Assert
+  const expectedResult = sampleArray.reduce(
+    (preValue, curValue) => preValue + curValue,
+    0
+  );
+  expect(testResult).toBe(expectedResult);
 });
